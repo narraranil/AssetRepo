435e3dff36f2093e4f5e21ad4616472a9db29ca2
RANIL <narraranil@users.noreply.github.com>
2019-08-06 14:28:16 +0800

    ee

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -3,5 +3,5 @@
 
 cd /opt/rancher
 #./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
-./rancher login https://k8s.eur.ad.sag/v3 --token token-2cr29:7krbsftjxj6fmzj4twk7lvrz2kpg7sqqjs2ks97j78cc79cnx9ckxq
+./rancher login https://k8s.eur.ad.sag/v3 --token token-k72zj:pbdbzjfxnr8ck4ffq7pgv2d8mb55krmnx26v4rm2qglgxb22h2kz2g
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
