92f616e2bf3661938b42d9d2bca0f255ed51b036
RANIL <narraranil@users.noreply.github.com>
2019-08-06 13:32:33 +0800

    new cluster

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,6 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+#./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+./rancher login https://k8s.eur.ad.sag/v3 --token token-2cr29:7krbsftjxj6fmzj4twk7lvrz2kpg7sqqjs2ks97j78cc79cnx9ckxq
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
