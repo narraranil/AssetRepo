7e2166b3ce2140dcbc331b6db570755761847ca2
RANIL <narraranil@users.noreply.github.com>
2020-02-23 14:46:45 +0800

    Microgateway

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -3,7 +3,7 @@
 
 cd /opt/rancher
 #./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
-./rancher login https://k8s.eur.ad.sag/v3 --token token-k72zj:pbdbzjfxnr8ck4ffq7pgv2d8mb55krmnx26v4rm2qglgxb22h2kz2g
+./rancher login https://k8s.eur.ad.sag/v3 --token token-qblmm:q4x9k8jbjpvdhgkp6bqj9k9w2wlrgstdzpr5fnt5z8bh6tc9gqfbmf
 #./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
 ./rancher kubectl replace -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
 
